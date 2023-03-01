f4766626f916ec509e0392fd5197fc6fddcf242c
Valters Vīrs <125027202+Valters-Virs@users.noreply.github.com>
2023-03-01 11:11:11 +0000

    update

--- tree_height.py
+++ tree_height.py
@@ -2,24 +2,75 @@
 
 import sys
 import threading
+import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
-    # Your code here
+    heights = numpy.zeros(n, int)
+
+    for i in range(n):
+        if parents[i] == -1:
+            heights[i] = 1
+        elif heights[parents[i]] != 0:
+            heights[i] = heights[parents[i]] + 1
+        else:
+            parent = parents[i]
+            cur_max_height = 1
+
+            while parent != -1:
+                cur_max_height += 1
+                parent = parents[parent]
+
+            parent = parents[i]
+            heights[i] = cur_max_height
+            
+            for j in range(cur_max_height - 1):
+                heights[parent] = cur_max_height - 1 - j
+                parent = parents[parent]
+
+    max_height = numpy.amax(heights)
+
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    input_type = input()
+
+    if input_type.upper()[0] == "I":
+        # Input I
+        count_input = input()
+        parent_input = input()
+
+        try:
+            parents = numpy.array(list(map(int, parent_input.split())))
+            count = int(count_input)
+        except:
+            print("Wrong input")
+        else:
+            if parents.size == count:
+                print(compute_height(count, parents))
+            else:
+                print("Number of nodes isn't matching")
+
+    elif input_type.upper()[0] == "F":
+        # Input F
+        file_input = input()
+        
+        try:
+            file = open("test/" + file_input[:2], "r")
+        except:
+            print("File not found")
+        else:
+            count = int(file.readline())
+            parents = numpy.array(list(map(int, file.readline().split())))
+
+            print(compute_height(count, parents))
+
+            file.close()
+    else:
+        print("Wrong input")
+
 
 
 # In Python, the default limit on recursion depth is rather low,
